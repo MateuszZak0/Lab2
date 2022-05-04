7a4b445ea0ebce9b30ed81285d6d3b127103590e
MateuszZak0 <akmateusz4@gamil.com>
2022-05-04 18:32:47 +0200

    Lab2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.lab2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens pl.lublin.wsei.java.cwiczenia to javafx.fxml;
+    exports pl.lublin.wsei.java.cwiczenia;
+    exports pl.lublin.wsei.java.cwiczenia.test;
+    opens pl.lublin.wsei.java.cwiczenia.test to javafx.fxml;
+}
