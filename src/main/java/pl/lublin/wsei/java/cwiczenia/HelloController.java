e022b0e4e894280bb12a3ec1e4b8588b966a0a5a
MateuszZak0 <akmateusz4@gamil.com>
2022-05-04 19:39:46 +0200

    Lab2b Punkt 3.f.Generowanie deklaracji referencji lbFile.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,14 +1,13 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label lbFile;
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
     }
 }
